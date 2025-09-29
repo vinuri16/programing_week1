3c816a25542b21e94f734a30c55fcd758d62eda2
vinuri16 <20067110@mydbs.ie>
2025-09-29 23:25:34 +0100

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,3 +1,16 @@
 //write a function to alert if overweight (15kg) baggage
+function check() {
+    const weightInput = document.getElementById('weight').value;
+    const weight = parseFloat(weightInput);
 
-//call it from the page
+    if (isNaN(weight)) {
+        alert("Please enter a valid number for baggage weight.");
+        return;
+    }
+
+    if (weight > 15) {
+        alert("Overweight baggage! (" + weight + " kg)");
+    } else {
+        alert("Baggage weight is OK. (" + weight + " kg)");
+    }
+}
