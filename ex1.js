fa7f07f125962353b5c8dec5c6dd31514f76d265
vinuri16 <20067110@mydbs.ie>
2025-09-30 15:07:40 +0100

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,4 +1,4 @@
-//write a function to alert if overweight (15kg) baggage
+// Function to check baggage weight
 function check() {
     const weightInput = document.getElementById('weight').value;
     const weight = parseFloat(weightInput);
@@ -8,8 +8,12 @@ function check() {
         return;
     }
 
-    if (weight > 15) {
-        alert("Overweight baggage! (" + weight + " kg)");
+    const limit = 15;
+
+    if (weight > limit) {
+        const excess = weight - limit;
+        alert("Overweight baggage! (" + weight + " kg)\n" +
+              "You need to reduce " + excess + " kg to meet the " + limit + " kg limit.");
     } else {
         alert("Baggage weight is OK. (" + weight + " kg)");
     }
